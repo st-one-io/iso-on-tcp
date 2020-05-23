b76c60ce0889534449765eca9727b8518d1bfc92
Guilherme Francescon <guilherme.francescon@netsmarttech.com>
2020-05-23 12:25:59 -0300

    Adds missing boundary checks on parser

--- src/parser.js
+++ src/parser.js
@@ -55,6 +55,8 @@ class ISOOnTCPParser extends Transform {
             let tpkt_reserved = chunk.readUInt8(ptr + 1);
             let tpkt_length = chunk.readUInt16BE(ptr + 2);
 
+            let tpktEnd = tpktStart + tpkt_length;
+
             //we don't have enough data, let's backup it
             if (chunk.length < (ptr + tpkt_length)) {
                 this._nBuffer = chunk.slice(ptr);
@@ -77,6 +79,14 @@ class ISOOnTCPParser extends Transform {
             let tpdu_length = chunk.readUInt8(ptr) + 1; //+1, because the length itself is not included in protocol
             ptr += 1;
 
+            let tpduEnd = tpduStart + tpdu_length;
+
+            // test if TPDU header is within TPKT boundaries
+            if (tpduEnd > tpktEnd){
+                cb(new Error(`TPDU header length [${tpdu_length}] out of bounds, TPKT lenght is [${tpkt_length}]`));
+                return;
+            }
+
             // TPDU - fixed part
 
             let type_and_credit = chunk.readUInt8(ptr);
@@ -89,6 +99,11 @@ class ISOOnTCPParser extends Transform {
                 case constants.tpdu_type.CR:
                 case constants.tpdu_type.CC:
                 case constants.tpdu_type.DR:
+
+                    if (tpduEnd - ptr < 5) {
+                        cb(new Error(`Not enough bytes for parsing TPDU header of type [${obj.type}], needs 5, has [${tpduEnd - ptr}]`));
+                        return;
+                    }
 
                     obj.destination = chunk.readUInt16BE(ptr);
                     ptr += 2;
@@ -110,6 +125,11 @@ class ISOOnTCPParser extends Transform {
                 case constants.tpdu_type.DT:
                 case constants.tpdu_type.ED:
 
+                    if (tpduEnd - ptr < 1) {
+                        cb(new Error(`Not enough bytes for parsing TPDU header of type [${obj.type}], needs 1, has [${tpduEnd - ptr}]`));
+                        return;
+                    }
+
                     let nr_and_eot = chunk.readUInt8(ptr);
                     obj.tpdu_number = nr_and_eot & 0x7f;
                     obj.last_data_unit = (nr_and_eot & 0x80) > 0;
@@ -127,12 +147,22 @@ class ISOOnTCPParser extends Transform {
 
             let var_params = [];
 
-            //TODO improvement: do boundary checks while parsing the variable data fields
             while ((ptr - tpduStart) < tpdu_length) {
+                if (tpduEnd - ptr < 2) {
+                    cb(new Error(`Not enough bytes for TPDU variable part header, ptr [${ptr}], start [${tpduStart}], length [${tpdu_length}]`));
+                    return;
+                }
+
                 let var_code = chunk.readUInt8(ptr);
                 ptr += 1;
                 let var_length = chunk.readUInt8(ptr);
                 ptr += 1;
+
+                if (tpduEnd - ptr < var_length) {
+                    cb(new Error(`Not enough bytes for TPDU variable part item, ptr [${ptr}], start [${tpduStart}], length [${tpdu_length}]`));
+                    return;
+                }
+
                 var_params.push({
                     code: var_code,
                     data: chunk.slice(ptr, ptr + var_length)
@@ -153,23 +183,17 @@ class ISOOnTCPParser extends Transform {
                         break;
                     default:
                         //for now, throw if we don't have it implemented
-                        cb(new Error(`Unknown or not implemented variable parameter code [${var_code}]:[${constants.var_type_desc[var_code]}]`));
+                        cb(new Error(`Unknown or not implemented variable parameter code [${elm.code}]:[${constants.var_type_desc[elm.code]}]`));
                         return;
                 }
             }
 
-            // TPDU - user data
-
-            let payloadStart = tpduStart + tpdu_length; //inclusive
-            let payloadEnd = tpktStart + tpkt_length; //exclusive
-            if (payloadEnd >= payloadStart) {
-                obj.payload = chunk.slice(payloadStart, payloadEnd);
-            }
+            // TPDU - user data - data between tpduEnd and tpktEnd
+            obj.payload = chunk.slice(tpduEnd, tpktEnd);
 
             this.push(obj);
 
-            //TODO should we do this, albeit technically correct?
-            ptr = payloadEnd;
+            ptr = tpktEnd;
         }
 
         cb();
